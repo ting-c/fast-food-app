45d35c1dd0f91f8ee26833edf0ff613d8da1dd24
ting-c <ting.cheong31@gmail.com>
2020-05-13 12:52:13 +0100

    user profile page unchanged

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
 // receive props from redux then display user details
-const UserProfile = ({currentUser}) => {
-  return (    
-      <div>
-        User Profile
-      </div>    
+const UserProfile = ({ currentUser }) => {
+	return (
+    <div>
+      
+    </div>
   )
 };
 
+const mapStateToProps = (state) => ({
+	currentUser: state.user.currentUser,
+});
 
-const mapStateToProps = (state) => (
-  {
-    currentUser: state.user.currentUser
-  }
-);
-
-export default connect(mapStateToProps)(UserProfile)
+export default connect(mapStateToProps)(UserProfile);
