a7540a9df6ba61bc9719664781fdaa5bc05bc456
ting-c <ting.cheong31@gmail.com>
2020-05-14 16:36:34 +0100

    connected Navbar cart dropdown to redux state

---
+++ src/components/CartItem/CartItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CartItem.styles.scss';
+
+const CartItem = ({ imageUrl, price, name, quantity} ) => (
+  <div className="CartItem">
+    <img src={imageUrl} alt='item' className='ItemImage'/>
+    <div className="ItemDetails">
+      <span className='name'>{name}</span>
+      <span className='price'>{quantity} x £{price}</span>
+    </div>
+  </div>
+)
+
+export default CartItem;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,45 +7,52 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import PropTypes from 'prop-types';
+import CartItem from './../CartItem/CartItem';
+import ListGroup from 'react-bootstrap/ListGroup';
 
+const NavBar = ({ currentUser, cartItems }) => {
+  const totalItems = cartItems.reduce( (acc, item) => {
+    return acc + item.quantity 
+  }, 0 );
 
-const NavBar = ({ currentUser }) => {
-  
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">
-        <img className="logo" src={Logo} alt="logo" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav href="#home">
-            { currentUser ? (
-              <div data-test='SignedInLinks'>
-                <Nav.Link href="#" onClick={() => auth.signOut()}>
-                  Sign Out
-                </Nav.Link>
-                <Nav.Link href="/userprofile" >
-                  Profile
-                </Nav.Link>
-                <NavDropdown title="Cart" id="cart-dropdown">
-                  <NavDropdown.Item >Item</NavDropdown.Item>
-                  <NavDropdown.Item >Checkout</NavDropdown.Item>
-                </NavDropdown>
-              </div>
-              ) : (
-                <div data-test='SignedOutLinks'>
-                  <Nav.Link href="/signin">
-                    Sign In
-                  </Nav.Link> 
-                </div> 
-              )
-            }
-          </Nav>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  )
+		<Navbar bg="light" expand="lg">
+			<Navbar.Brand href="/">
+				<img className="logo" src={Logo} alt="logo" />
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<Nav href="#home">
+						{currentUser ? (
+							<div data-test="SignedInLinks">
+								<Nav.Link href="#" onClick={() => auth.signOut()}>
+									Sign Out
+								</Nav.Link>
+								<Nav.Link href="/userprofile">Profile</Nav.Link>
+								<NavDropdown title={`Cart (${totalItems})`} id="cart-dropdown">
+									<NavDropdown.Item>
+										<ListGroup as="ul">
+											{cartItems.map( cartItem => (
+												<ListGroup.Item as="li">
+                          <CartItem key={cartItem.id} {...cartItem} />
+                        </ListGroup.Item>
+											))}
+										</ListGroup>
+									</NavDropdown.Item>
+									<NavDropdown.Item>Checkout</NavDropdown.Item>
+								</NavDropdown>
+							</div>
+						) : (
+							<div data-test="SignedOutLinks">
+								<Nav.Link href="/signin">Sign In</Nav.Link>
+							</div>
+						)}
+					</Nav>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 };
 
 NavBar.propTypes = {
@@ -53,7 +60,8 @@ NavBar.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+  currentUser: state.user.currentUser,
+  cartItems: state.cart.cartItems
 })
 
 
