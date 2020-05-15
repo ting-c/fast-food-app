f130763c572f85b49a45c7ce5e0403420bfee6f6
ting-c <ting.cheong31@gmail.com>
2020-05-15 18:28:10 +0100

    updated uer profile page and font style

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -1,17 +1,55 @@
 import React from "react";
+import './UserProfile.styles.scss';
 import { connect } from "react-redux";
+import { currentUserSelector } from "../../redux/selectors/userSelectors";
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // receive props from redux then display user details
 const UserProfile = ({ currentUser }) => {
-	return (
-    <div>
-      
-    </div>
-  )
-};
+  if (!currentUser) return (
+        <div> Loading ... </div> )
 
-const mapStateToProps = (state) => ({
-	currentUser: state.user.currentUser,
+  const { name, email, createdAt } = currentUser
+  return (
+		<Form className='UserProfile'>
+
+			<Form.Group as={Row} >
+				<Form.Label column sm="2">
+					Name
+				</Form.Label>
+				<Col sm="10">
+					<Form.Control plaintext readOnly defaultValue={name} />
+				</Col>
+			</Form.Group>
+
+			<Form.Group as={Row} >
+				<Form.Label column sm="2">
+					Email
+				</Form.Label>
+				<Col sm="10">
+					<Form.Control plaintext readOnly defaultValue={email} />
+				</Col>
+			</Form.Group>
+
+			<Form.Group as={Row} >
+				<Form.Label column sm="2">
+					Created At
+				</Form.Label>
+				<Col sm="10">
+					<Form.Control id='date' plaintext readOnly defaultValue={createdAt.toDate()} />
+				</Col>
+			</Form.Group>
+
+
+		</Form>
+	);
+    
+}
+
+const mapStateToProps = state => ({
+	currentUser: currentUserSelector(state)
 });
 
 export default connect(mapStateToProps)(UserProfile);
