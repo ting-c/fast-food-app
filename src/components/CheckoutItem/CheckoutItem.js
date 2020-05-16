250a6f20af3978d0eaa585af7c1162cea386c39f
ting-c <ting.cheong31@gmail.com>
2020-05-16 19:59:43 +0100

    added add and delete buttons for items on checkout page

--- src/components/CheckoutItem/CheckoutItem.js
+++ src/components/CheckoutItem/CheckoutItem.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import './CheckoutItem.styles.scss';
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { connect } from 'react-redux';
+import { addItem, deleteItem } from './../../redux/actions/cartActions';
 
-const CheckoutItem = ({item}) => {
+const CheckoutItem = ({item, addItem, deleteItem}) => {
   const {imageUrl, name, quantity, price} = item;
   return (
-			<Row className='CheckoutItem'>
-				<Col className="itemImage">
-					<img src={imageUrl} alt={name} />
-				</Col>
-				<Col className="description" xs={4}>
-					{name}
-				</Col>
-				<Col className="quantity">{quantity}</Col>
-				<Col className="price">{price}</Col>
-			</Row>
+		<Row className="CheckoutItem">
+			<Col className="itemImage">
+				<img src={imageUrl} alt={name} />
+			</Col>
+			<Col className="description" xs={4}>
+				{name}
+			</Col>
+			<Col className="quantity">
+				<span className='delete' onClick={() => deleteItem(item)}>{"<"}</span>
+				<span className='itemCount'>{quantity}</span>
+				<span className='add' onClick={() => addItem(item)}>{">"}</span>
+			</Col>
+			<Col className="price">{quantity * price}</Col>
+		</Row>
 	);
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+	addItem: item => dispatch(addItem(item)),
+	deleteItem: item => dispatch(deleteItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
