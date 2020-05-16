50e721125938a2b1c18390ed0f3c10f5a1d43925
ting-c <ting.cheong31@gmail.com>
2020-05-16 09:16:28 +0100

    changed item price data type to number

--- src/items-store/foodItems.js
+++ src/items-store/foodItems.js
@@ -3,7 +3,7 @@ const foodItems = [
 		id: 0,
 		section: "burger",
 		name: "beef burger",
-		price: "20",
+		price: 20,
 		imageUrl:
 			"https://images.unsplash.com/photo-1587841424505-4205a6e73ef7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
 	},
@@ -11,7 +11,7 @@ const foodItems = [
 		id: 1,
 		section: "salad",
 		name: "mixed salad",
-		price: "10",
+		price: 10,
 		imageUrl:
 			"https://images.unsplash.com/photo-1547496502-affa22d38842?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=884&q=80",
 	},
@@ -19,7 +19,7 @@ const foodItems = [
 		id: 2,
 		section: "burger",
 		name: "cheese burger",
-		price: "30",
+		price: 30,
 		imageUrl:
 			"https://images.unsplash.com/photo-1562817981-221763b0c0e5?ixlib=rb-1.2.1&auto=format&fit=crop&w=740&q=80",
 	},
@@ -27,7 +27,7 @@ const foodItems = [
 		id: 3,
 		section: "sides",
 		name: "french fries",
-		price: "15",
+		price: 15,
 		imageUrl:
 			"https://images.unsplash.com/photo-1518013431117-eb1465fa5752?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
 	},
@@ -35,7 +35,7 @@ const foodItems = [
 		id: 4,
 		section: "sides",
 		name: "nachos",
-		price: "12",
+		price: 12,
 		imageUrl:
 			"https://images.unsplash.com/photo-1582169296194-e4d644c48063?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=903&q=80",
 	},
@@ -43,7 +43,7 @@ const foodItems = [
 		id: 4,
 		section: "doughnuts",
 		name: "chocolate doughnut",
-		price: "15",
+		price: 15,
 		imageUrl:
 			"https://images.unsplash.com/photo-1530016910220-faf7fab2125c?ixlib=rb-1.2.1&auto=format&fit=crop&w=697&q=80",
 	},
@@ -51,7 +51,7 @@ const foodItems = [
 		id: 5,
 		section: "doughnuts",
 		name: "cookies and cream doughnut",
-		price: "15",
+		price: 15,
 		imageUrl:
 			"https://images.unsplash.com/photo-1533136872-c5d7b7b800ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
 	},
@@ -59,7 +59,7 @@ const foodItems = [
 		id: 6,
 		section: "milkshake",
 		name: "chocolate milkshake",
-		price: "15",
+		price: 15,
 		imageUrl:
 			"https://images.unsplash.com/photo-1572490122747-3968b75cc699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
 	},
@@ -67,7 +67,7 @@ const foodItems = [
 		id: 7,
 		section: "milkshake",
 		name: "strawberry milkshake",
-		price: "15",
+		price: 15,
 		imageUrl:
 			"https://images.unsplash.com/photo-1579954115545-a95591f28bfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
 	},
