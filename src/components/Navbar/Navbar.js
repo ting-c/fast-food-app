2cd49b8be8c35264f2719ba2b5263b113db203ee
ting-c <ting.cheong31@gmail.com>
2020-05-16 13:01:11 +0100

    moved checkout button location and used history prop to navigate in navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './NavBar.styles.scss';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase';
+import { withRouter } from 'react-router-dom';
 import Logo from '../../img/Logo.png';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -13,42 +14,55 @@ import Button from 'react-bootstrap/Button';
 import { cartItemsSelector, totalItemsSelector } from '../../redux/selectors/cartSelectors';
 import { currentUserSelector } from '../../redux/selectors/userSelectors';
 
-const NavBar = ({ currentUser, cartItems, totalItems }) => {
+const NavBar = ({ currentUser, cartItems, totalItems, history }) => {
 
   return (
 		<Navbar bg="light" expand="lg">
-			<Navbar.Brand href="/">
+			<Navbar.Brand onClick={() => history.push("/")}>
 				<img className="logo" src={Logo} alt="logo" />
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav className="mr-auto">
-					<Nav href="#home">
+					<Nav>
 						{currentUser ? (
 							<div data-test="SignedInLinks">
-								<Nav.Link href="#" onClick={() => auth.signOut()}>
-									Sign Out
+								<Nav.Link onClick={() => auth.signOut()}>Sign Out</Nav.Link>
+								<Nav.Link onClick={() => history.push("/userprofile")}>
+									Profile
 								</Nav.Link>
-								<Nav.Link href="/userprofile">Profile</Nav.Link>
 								<NavDropdown title={`Cart (${totalItems})`} id="cart-dropdown">
 									<NavDropdown.Item>
-											<ListGroup as="ul">
-												{ cartItems.length ? cartItems.map( cartItem => (
-														<ListGroup.Item as="li" key={cartItem.id}>
-															<CartItem {...cartItem} />
-														</ListGroup.Item>
-												)) : <span className='emptyCart'>Your cart is currently empty</span>}
-											</ListGroup>
-									</NavDropdown.Item>
-									<NavDropdown.Item>
-										{ cartItems.length ? <Button variant="success">Checkout</Button>
-												: null }
+										<ListGroup as="ul">
+											{cartItems.length ? (
+												cartItems.map((cartItem) => (
+													<ListGroup.Item as="li" key={cartItem.id}>
+														<CartItem {...cartItem} />
+													</ListGroup.Item>
+												))
+											) : (
+												<span className="emptyCart">
+													Your cart is currently empty
+												</span>
+											)}
+										</ListGroup>
+										{cartItems.length ? (
+											<Button
+												className="checkoutButton"
+												variant="success"
+												onClick={() => history.push("/checkout")}
+											>
+												Checkout
+											</Button>
+										) : null}
 									</NavDropdown.Item>
 								</NavDropdown>
 							</div>
 						) : (
 							<div data-test="SignedOutLinks">
-								<Nav.Link href="/signin">Sign In</Nav.Link>
+								<Nav.Link onClick={() => history.push("/signin")}>
+									Sign In
+								</Nav.Link>
 							</div>
 						)}
 					</Nav>
@@ -70,4 +84,4 @@ const mapStateToProps = state => ({
 
 
 export { NavBar };
-export default connect(mapStateToProps)(NavBar);
+export default withRouter(connect(mapStateToProps)(NavBar));
