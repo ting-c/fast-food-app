75b4894aeaeac064a98327167830d14aa74f7c55
ting-c <ting.cheong31@gmail.com>
2020-05-17 15:40:12 +0100

    restructured foodItem data store, added a menu button on item page

--- src/pages/ItemPage/ItemPage.js
+++ src/pages/ItemPage/ItemPage.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import './ItemPage.styles.scss';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import {foodItemsSelector} from "../../redux/selectors/foodItemsSelector";
 import Item from '../../components/Item/Item';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 
-const ItemPage = ({foodItems, match}) => {
-	const itemList = foodItems
-		.filter((item) => item.section === match.params.menuId)
+const ItemPage = ({foodItems, match, history}) => {
+	const itemList = foodItems[match.params.menuId]
 		.map((item) => {
 			return <Item key={item.id} item={item} />;
     });
@@ -17,6 +19,9 @@ const ItemPage = ({foodItems, match}) => {
   return (		
 			<Container className='ItemPage' data-test='ItemPageComponent'>
 				<Row className='justify-content-around align-content-between flex-wrap p-3 m-1'>
+					<Button className='backToMenu' onClick={() => history.push('/menu')}>
+						Menu
+					</Button>
 					{itemList}
 				</Row>
 			</Container>		
@@ -27,4 +32,4 @@ const mapStateToProps = createStructuredSelector({
 	foodItems : foodItemsSelector
 });
 
-export default connect(mapStateToProps)(ItemPage);
+export default withRouter(connect(mapStateToProps)(ItemPage));
--- src/redux/items-store/foodItems.js
+++ src/redux/items-store/foodItems.js
@@ -1,63 +1,57 @@
-const foodItems = [
-	{
-		id: 0,
-		section: "burger",
-		name: "beef burger",
-		price: 6.50,
-		imageUrl:
-			"https://images.unsplash.com/photo-1587841424505-4205a6e73ef7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-	},
-	{
-		id: 1,
-		section: "salad",
-		name: "mixed salad",
-		price: 2.40,
-		imageUrl:
-			"https://images.unsplash.com/photo-1547496502-affa22d38842?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=884&q=80",
-	},
-	{
-		id: 2,
-		section: "burger",
-		name: "cheese burger",
-		price: 5.50,
-		imageUrl:
-			"https://images.unsplash.com/photo-1562817981-221763b0c0e5?ixlib=rb-1.2.1&auto=format&fit=crop&w=740&q=80",
-	},
-	{
-		id: 3,
-		section: "sides",
-		name: "french fries",
-		price: 1.20,
-		imageUrl:
-			"https://images.unsplash.com/photo-1518013431117-eb1465fa5752?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-	},
-	{
-		id: 4,
-		section: "sides",
-		name: "nachos",
-		price: 2.10,
-		imageUrl:
-			"https://images.unsplash.com/photo-1582169296194-e4d644c48063?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=903&q=80",
-	},
-	{
-		id: 4,
-		section: "doughnuts",
+const foodItems = {
+	burger: [
+		{	id: 0,
+			name: "beef burger",
+			price: 6.50,
+			imageUrl:
+				"https://images.unsplash.com/photo-1587841424505-4205a6e73ef7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+		},
+		{	id: 1,
+			name: "cheese burger",
+			price: 5.50,
+			imageUrl:
+				"https://images.unsplash.com/photo-1562817981-221763b0c0e5?ixlib=rb-1.2.1&auto=format&fit=crop&w=740&q=80"
+		}],
+
+	salad: [
+		{	id: 2,
+			name: "mixed salad",
+			price: 2.40,
+			imageUrl:
+				"https://images.unsplash.com/photo-1547496502-affa22d38842?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=884&q=80"
+		}],
+	
+	sides: [
+		{ id: 3,
+			name: "french fries",
+			price: 1.20,
+			imageUrl:
+				"https://images.unsplash.com/photo-1518013431117-eb1465fa5752?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+		},
+		{	id: 4,
+			section: "sides",
+			name: "nachos",
+			price: 2.10,
+			imageUrl:
+				"https://images.unsplash.com/photo-1582169296194-e4d644c48063?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=903&q=80"
+		}],
+	
+	doughnuts: [	
+	{ id: 4,
 		name: "chocolate doughnut",
 		price: 2.40,
 		imageUrl:
 			"https://images.unsplash.com/photo-1530016910220-faf7fab2125c?ixlib=rb-1.2.1&auto=format&fit=crop&w=697&q=80",
 	},
-	{
-		id: 5,
-		section: "doughnuts",
+	{ id: 5,
 		name: "cookies and cream doughnut",
 		price: 2.40,
 		imageUrl:
 			"https://images.unsplash.com/photo-1533136872-c5d7b7b800ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-	},
-	{
-		id: 6,
-		section: "milkshake",
+	}],
+
+	milkshake: [
+	{ id: 6,
 		name: "chocolate milkshake",
 		price: 2.60,
 		imageUrl:
@@ -65,12 +59,11 @@ const foodItems = [
 	},
 	{
 		id: 7,
-		section: "milkshake",
 		name: "strawberry milkshake",
 		price: 2.60,
 		imageUrl:
 			"https://images.unsplash.com/photo-1579954115545-a95591f28bfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-	},
-];
+	}]
+};
 
 export default foodItems
