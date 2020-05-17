a21725d93838980e4dcde6337616777899732c5e
ting-c <ting.cheong31@gmail.com>
2020-05-17 08:50:36 +0100

    updated opening times page

---
+++ src/items-store/openingTimes.js
@@ -0,0 +1,18 @@
+const openingTimes = [
+  { Day: 'Monday',
+    Time: '9:00am - 5:30pm' },
+  { Day: 'Tuesday',
+    Time: '9:00am - 5:30pm' },
+  { Day: 'Wednesday',
+    Time: '9:00am - 5:30pm' },
+  { Day: 'Thursday',
+    Time: '9:00am - 5:30pm' },
+  { Day: 'Friday',
+    Time: '9:00am - 5:30pm' },
+  { Day: 'Saturday',
+    Time: '10:00am - 5:30pm' },
+  { Day: 'Sunday',
+    Time: 'Closed' }  
+];
+
+export default openingTimes;
--- src/pages/OpeningTimes/OpeningTimes.js
+++ src/pages/OpeningTimes/OpeningTimes.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import './OpeningTimes.styles.scss'
+import './OpeningTimes.styles.scss';
+import openingTimes from './../../items-store/openingTimes';
+import Card from 'react-bootstrap/Card';
 
 const OpeningTimes = () => (
   <div className='OpeningTimes'>
-    <h2>OpeningTimes</h2>
+    <Card className='Card'>
+      <Card.Body>
+        <Card.Title className='title'>
+          Opening Times
+        </Card.Title>
+        <Card.Text>
+          <ul>
+          {
+            openingTimes.map( day => (
+              <li className='listItem'>
+                <span className='day'>{day.Day} :</span>
+                <span className='time'>{day.Time}</span>
+              </li>             
+            ))
+          }
+          </ul>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   </div>
 )
 
