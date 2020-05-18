8766328a556cb66a08f989d58eb02fe95f4dde24
ting-c <ting.cheong31@gmail.com>
2020-05-18 22:25:12 +0100

    updated style for checkout item and updated test for item page

--- src/pages/ItemPage/ItemPage.test.js
+++ src/pages/ItemPage/ItemPage.test.js
@@ -4,25 +4,31 @@ import { ItemPage } from "./ItemPage";
 import { shallow } from "enzyme";
 import { findByTestAttr } from "./../../testingUtils";
 
-const setUp = (props = {}) => {
-	const component = shallow(<ItemPage {...props} />);
-	return component;
-};
-
 describe("Item Page Component", () => {
 
-  const expectedProps = {
-    match : {
-      params: {
-        url: 'testing.com'
-      } 
-		},
-		foodItems: ['item_1', 'item_2']
-  }
+  
 
 	let component;
 	beforeEach(() => {
-		component = setUp(expectedProps);
+
+		const item_1 = {
+			id: 1
+		}
+
+		const expectedProps = {
+				match : {
+					params: {
+						menuId: 1
+					} 
+				},
+				foodItems : {
+					'item_1': {},
+					'item_2': {}
+				}
+		};
+
+		component = shallow(<ItemPage {...expectedProps} />);
+
 	});
 
 	it("Should render without errors", () => {
